f59256db300258c99e6c6d4b7e692c3cf7c317ef
André Miranda <andremiramor@gmail.com>
2015-05-14 23:28:35 -0300

    Adicionada versão do app discutida

--- models/document.rb
+++
@@ -1,9 +0,0 @@
-class Document
-
-	attr_accessor :id, :template, :document
-	
-	def initialize(template, document)
-		@template, @document = template, document
-	end
-
-end
--- models/field.rb
+++
@@ -1,9 +0,0 @@
-class Field
-
-	attr_accessor :id, :type, :name, :value
-
-	def initialize(type, name)
-		@type, @name = type, name
-	end
-
-end
