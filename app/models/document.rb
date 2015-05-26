b9d7dc60369803a4b209101c03a731e232008d75
André Miranda <andremiramor@gmail.com>
2015-05-26 07:33:20 -0300

    Added buggy gridfs support

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,4 +2,5 @@ class Document
   include Mongoid::Document
   field :name, type: String
   field :description, type: String
+  mount_uploader :file_name, DocumentUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :templates
   resources :documents
+  match "/documents/uploads/*path" => "gridfs#serve"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
