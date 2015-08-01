08b23f2a1de03c68d2f6e876166d105680dbde7a
caio <caio.cmsilva@gmail.com>
2015-08-01 13:25:59 -0300

    removing ruby files

--- app/models/template.rb
+++
@@ -1,5 +0,0 @@
-class Template
-  include Mongoid::Document
-  field :name, type: String
-  field :fields, type: Array
-end
--- app/views/templates/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @template, :id, :name, :fields, :created_at, :updated_at
